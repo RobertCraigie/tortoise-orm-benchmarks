7e5afc185c1b15f8b9649621d34041ac81db0948
Robert Craigie <robertcraigie30@gmail.com>
2021-12-03 19:23:11 +0000

    try running postgres too

--- benchmarks/bench_all.sh
+++ benchmarks/bench_all.sh
@@ -7,7 +7,7 @@ cd $(dirname $0)
 echo > results
 
 DBTYPE=sqlite ./bench.sh $1
-# python -V | grep PyPy || DBTYPE=postgres ./bench.sh $1
+python -V | grep PyPy || DBTYPE=postgres ./bench.sh $1
 
 # TODO
 # DBTYPE=mysql ./bench.sh $1
