56a76334a24f3d99a04584a6e796ad2c00f87750
Robert Craigie <robertcraigie30@gmail.com>
2021-12-04 11:43:30 +0000

    fix

--- benchmarks/clean.sql
+++ benchmarks/clean.sql
@@ -1,13 +1,1 @@
-do
-$$
-declare
-  l_stmt text;
-begin
-  select 'truncate ' || string_agg(format('%I.%I', schemaname, tablename), ',')
-    into l_stmt
-  from pg_tables
-  where schemaname in ('public');
-
-  execute l_stmt;
-end;
-$$
+select 'drop table if exists "' || tablename || '" cascade;' from pg_tables;
