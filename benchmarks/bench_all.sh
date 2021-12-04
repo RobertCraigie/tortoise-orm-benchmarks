e8f9582418fe3a95ac03b024730c11c592d01b9e
Robert Craigie <robertcraigie30@gmail.com>
2021-12-04 11:41:03 +0000

    test all postgres

--- benchmarks/bench_all.sh
+++ benchmarks/bench_all.sh
@@ -7,9 +7,7 @@ cd $(dirname $0)
 echo > results
 
 # DBTYPE=sqlite ./bench.sh $1
-# python -V | grep PyPy || DBTYPE=postgres ./bench.sh $1
-
-DBTYPE=postgres sh tortoise/bench.sh
+python -V | grep PyPy || DBTYPE=postgres ./bench.sh $1
 
 # TODO
 # DBTYPE=mysql ./bench.sh $1
