5f370f344139ff55e5e21b7f564d96a6f2520486
Robert Craigie <robertcraigie30@gmail.com>
2021-12-04 11:39:03 +0000

    update

--- benchmarks/db.sh
+++ benchmarks/db.sh
@@ -8,7 +8,8 @@ then
         psql -U postgres -w -c 'drop database tbench';
         psql -U postgres -w -c 'create database tbench';
     else
-        psql -U postgres -w -c 'SELECT "TRUNCATE " || input_table_name || " CASCADE;" AS truncate_query FROM(SELECT table_schema || "." || table_name AS input_table_name FROM information_schema.tables WHERE table_schema NOT IN ("pg_catalog", "information_schema") AND table_schema NOT LIKE "pg_toast%") AS information;';
+        # NOTE: assumes this script is being ran from a bench.sh script
+        psql -U postgres -w -f ../clean.sql;
     fi
 elif [ "$DBTYPE" = "mysql" ]
 then
